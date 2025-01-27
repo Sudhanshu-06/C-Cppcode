deee243c9ea2d3d124cbe7dda703784f32e7fbef
Sudhanshu-06 <sudhanshu767778@gmail.com>
2025-01-27 19:49:25 +0530

    done

---
+++ C++_Nexus/Day-2/even_no.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    for(num=1; num<=20; num++)
+        if(num%2==0)
+        {
+            cout<<num<<endl;
+        }
+    return 0;
+}
---
+++ C++_Nexus/Day-2/sum_1to_10.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num, sum=0;
+    for(num=1; num<=10; num++)
+    {
+        sum=sum+num;
+
+    }
+    cout<<sum<<endl;
+
+}
